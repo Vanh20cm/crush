0f0212a0b9e7ca28ff56592c564b7fe2dff670a6
Vanh20cm <lamhanhhatinh123@gmail.com>
2022-03-08 11:14:01 +0700

    finsh-project

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hello :33',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Dạ hehe :)))',
+    title: 'M thích t đk ? ',
+    desc: 'Thoát ra làm tó :((( ',
+    btnYes: 'Có nè :33',
+    btnNo: 'M mơ hả ??? ',
+    question:'Nói cho t biết đi vì sao m thích t 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửi :33 ! 💌💌',
+    reply: 'Tại vì cậu đẹp trai đó ahihi 😊😊',
+    mess: 'T biết mà, t cũng thích m lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho t nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/dautran.anh/'
+}
